1d6652aa198b57cb7d954e8baa0d33040295d6ba
rfmineguy <rfmineguy@gmail.com>
2023-07-02 11:50:19 -0700

    MASSIVE memory leak, fix it

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 #include <limits.h>
 #include <time.h>
 #include <SDL.h>
+#include <stdio.h>
 #include "ray.h"
 #include "boundary.h"
 #include "sdl_context.h"
 
 #define BOUNDARY_COUNT 7
+#define RAY_COUNT 7
 
 int random_range_i(int lower, int upper) {
   int random = rand() % (upper - lower);
@@ -58,9 +60,8 @@ void ray_game_mouse(sdl_context_t ctx, boundary_t boundaries[BOUNDARY_COUNT]) {
   }
 }
 
-void ray_game_angle(sdl_context_t ctx, boundary_t boundaries[BOUNDARY_COUNT]) {
-#define RAY_COUNT 7
-  static ray_t rays[RAY_COUNT] = {0};
+void ray_game_angle(sdl_context_t ctx, ray_t rays[RAY_COUNT], boundary_t boundaries[BOUNDARY_COUNT]) {
+  printf("Ray Game Angle\n");
   const Uint8* keystate = SDL_GetKeyboardState(NULL);
 
   // Movement
@@ -84,8 +85,11 @@ void ray_game_angle(sdl_context_t ctx, boundary_t boundaries[BOUNDARY_COUNT]) {
   }
 
   // Perform raycasting
+  int closest_boundaries[RAY_COUNT] = {-1};
+  int closest_distances[RAY_COUNT] = {INT_MAX};
   for (int i = 0; i < RAY_COUNT; i++) {
-    int closest_boundary = -1;
+    printf("Raycasting\n");
+    int closest_hit_x = 0, closest_hit_y = 0;
     int closest_boundary_dist = INT_MAX;
     for (int j = 0; j < BOUNDARY_COUNT; j++) {
       boundary_t* b = &boundaries[j];
@@ -93,34 +97,55 @@ void ray_game_angle(sdl_context_t ctx, boundary_t boundaries[BOUNDARY_COUNT]) {
       if (ray_cast_against(rays[i], *b, &hit_x, &hit_y)) {
         int hit_distance = (int)((hit_x-rays[i].x1) * (hit_x-rays[i].x1) + (hit_y-rays[i].y1) *(hit_y-rays[i].y1));
         if (hit_distance < closest_boundary_dist) {
-          boundaries[closest_boundary].hit = 0;
+          closest_boundaries[i] = j;
           closest_boundary_dist = hit_distance;
-          closest_boundary = j;
-          boundaries[closest_boundary].hit = 0;
-          SDL_Rect r = (SDL_Rect) {.x=hit_x,.y=hit_y,.w=10,.h=10};
-          SDL_RenderDrawRect(ctx.renderer, &r);
-          b->hit = 1;
+          closest_distances[i] = closest_boundary_dist;
+          closest_hit_x = hit_x;
+          closest_hit_y = hit_y;
         }
       }
     }
+    printf("Done Raycasting\n");
+
+    printf("Rendering...\n");
+    SDL_Rect rect = (SDL_Rect) {.x=closest_hit_x, .y=closest_hit_y, .w=10, .h=10};
+    SDL_RenderDrawRect(ctx.renderer, &rect);
   }
 
   // Visualize hits
-  for (int i = 0; i < BOUNDARY_COUNT; i++) {
-    boundary_t b = boundaries[i];
-    if (b.hit) {
+  for (int i = 0; i < RAY_COUNT; i++) {
+    int closest_boundary = closest_boundaries[i];
+    if (closest_boundary == -1) {
+      
+    }
+    boundary_t b = boundaries[closest_boundaries[i]];
+    if (closest_boundary == i && b.hit && closest_boundary >= 0) {
       SDL_SetRenderDrawColor(ctx.renderer, 0x00, 0xff, 0x00, 0xff);
     }
     else {
       SDL_SetRenderDrawColor(ctx.renderer, 0xff, 0x00, 0x00, 0xff);
     }
     SDL_RenderDrawLine(ctx.renderer, b.x1, b.y1, b.x2, b.y2);
-    boundaries[i].hit = 0;
-    boundaries[i].hit_by = -1;
+    /*
+    if (closest_boundary > 0 && b.hit && i == closest_boundary) {
+      SDL_SetRenderDrawColor(ctx.renderer, 0x00, 0xff, 0x00, 0xff);
+    }
+    else {
+      SDL_SetRenderDrawColor(ctx.renderer, 0xff, 0x00, 0x00, 0xff);
+    }
+    SDL_RenderDrawLine(ctx.renderer, b.x1, b.y1, b.x2, b.y2);
+    */
+  }
+
+  for (int i = 0; i < RAY_COUNT; i++) {
+    rays[i].has_hit = 0;
   }
 }
 
 int main() {
+  fprintf(stderr, "MASSIVE MEMORY LEAK\n");
+  return 342;
+
   sdl_context_t ctx = sdl_initialize(600, 600);
   if (ctx.result < 0) {
     sdl_deinitialize(ctx);
@@ -137,6 +162,14 @@ int main() {
     boundaries[i].y2 = random_range_i(0, 600);
   }
 
+  ray_t rays[RAY_COUNT] = {0};
+  for (int i = 0; i < RAY_COUNT; i++) {
+    double degrees = (180.0 / RAY_COUNT) * i;
+    printf("%f\n", degrees);
+    double rads = degrees * (M_PI / 180.0);
+    ray_update_angle(&rays[i], rads, 100);
+  }
+
   // GAME LOOP
   int running = 1;
   SDL_Event e;
@@ -150,7 +183,7 @@ int main() {
     SDL_RenderClear(ctx.renderer);
 
     // ray_game_mouse(ctx, boundaries);
-    ray_game_angle(ctx, boundaries);
+    ray_game_angle(ctx, rays, boundaries);
 
     SDL_RenderPresent(ctx.renderer);
   }
